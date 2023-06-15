3d565d9dd4c0aef00106bd735338371e6343d301
seth <107208142+socialsuicide@users.noreply.github.com>
2023-06-15 13:11:13 +1000

    Release v0.4c

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import getpass, subprocess, psutil, shutil, os, colorama, readchar
+import getpass, subprocess, psutil, shutil, os, colorama, readchar, requests
 from urllib.request import urlretrieve, urlopen
 from colorama import Fore
+from datetime import datetime
 from win32com.client import GetObject
 
 os.system("title Roblox 403 Fixer")
-data = []
 
 def menu():
     print(f"""
@@ -22,14 +22,15 @@ def menu():
 
     """)
 
-def fetch_version_data():
-    for i in urlopen("http://setup.roblox.com/DeployHistory.txt"):
-        if str(i).replace("b'", "").replace("\\r\\n'", "").startswith("New WindowsPlayer version-"):
-            data.append(str(i).replace("b'", "").replace("\\r\\n'", ""))
+def close_console():
+    for process in GetObject("winmgmts:").ExecQuery("select * from Win32_Process where Name = 'cmd.exe'"):
+        os.system(f"taskkill /pid {str(process.Properties_('ProcessId').Value)}")
 
 def get_version():
-    fetch_version_data()
-    return "\n".join(data).splitlines()[-1].split("New WindowsPlayer ")[1].split(" at ")[0]
+    for i in requests.get("https://api.whatexploitsare.online/status").json():
+        for name in i:
+            if name == "ROBLOX":
+                return i[name]["version"]
 
 def close_roblox():
     try:
@@ -38,14 +39,10 @@ def close_roblox():
     except:
         pass
 
-def close_console():
-    for process in GetObject("winmgmts:").ExecQuery("select * from Win32_Process where Name = 'cmd.exe'"):
-        os.system(f"taskkill /pid {str(process.Properties_('ProcessId').Value)}")
-
 # init
 if __name__ == "__main__":
     menu()
-    print(f"{Fore.LIGHTRED_EX}Currently checking for any active roblox tasks.\n")
+    print(f"{Fore.LIGHTRED_EX}[{datetime.now().strftime('%H:%M:%S')}] Currently checking for any active roblox tasks.\n")
 
     while True:
         if list(p.name() for p in psutil.process_iter()).count("RobloxPlayerBeta.exe") != 0:
@@ -54,18 +51,18 @@ if __name__ == "__main__":
             break
     
     try:
-        print(f"Attempting to remove old roblox tracer files.\n")
+        print(f"[{datetime.now().strftime('%H:%M:%S')}] Attempting to remove old roblox tracer files.\n")
         shutil.rmtree(f"C:\\Users\\{getpass.getuser()}\\AppData\\Local\\Roblox")
     except Exception as e:
         print(f"ERROR: {e}\n")
     
-    print(f"Fetching RobloxPlayerLauncher {get_version()}.\n")
+    print(f"[{datetime.now().strftime('%H:%M:%S')}] Fetching RobloxPlayerLauncher {get_version()}.\n")
     urlretrieve(f"https://setup.rbxcdn.com/{get_version()}-Roblox.exe", "RobloxPlayerLauncherFixer.exe")
     subprocess.call("RobloxPlayerLauncherFixer.exe", shell = True)
 
     os.startfile(r"uninstall.bat")
 
-    print(f"Error Code 403 has been successfully removed from RobloxPlayerLauncher {get_version()}.\n")
-    print("Press any button to close this console.\n")
+    print(f"[{datetime.now().strftime('%H:%M:%S')}] Error Code 403 has been successfully removed from RobloxPlayerLauncher {get_version()}.\n")
+    print(f"[{datetime.now().strftime('%H:%M:%S')}] Press any button to close this console.\n")
     if readchar.readchar():
         close_console()
